ea0e79dac7a6ddef0d728e07ad110bcb909b3027
ddyggu <ddyggu@naver.com>
2024-09-22 23:48:02 +0900

    Security 설정 변경

--- ADMIN/src/main/java/com/e9pay/common/SecurityConfiguration.java
+++ ADMIN/src/main/java/com/e9pay/common/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,28 +14,23 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/login");
-        http.securityMatcher("/admin/**", "/")
-            .authorizeHttpRequests((authorize) -> authorize
-                        .shouldFilterAllDispatcherTypes(true)
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().authenticated());
-        return http.build();
-    }
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/css/**", "/js/**").permitAll() // Allow public access
+                        .anyRequest().authenticated() // All other requests require authentication
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Custom login page URL
+                        .permitAll() // Allow all to access the login page
+                        .defaultSuccessUrl("/home", true) // Redirect after successful login
+                        .failureUrl("/login?error=true") // Redirect after failed login
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // Logout URL
+                        .logoutSuccessUrl("/login?logout=true") // Redirect after logout
+                );
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain resourceFilTerChain(HttpSecurity http) throws Exception {
-        http.securityMatcher("/static/**")
-            .authorizeHttpRequests((authorize) -> authorize
-                        .shouldFilterAllDispatcherTypes(true)
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().permitAll())
-            .requestCache().disable()
-            .securityContext().disable()
-            .sessionManagement().disable();
         return http.build();
     }
 
